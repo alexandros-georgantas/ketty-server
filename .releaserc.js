c4675599a56300f6dcee5385054a8990441507cb
Yannis Barlas <yannisbarlas@gmail.com>
2023-03-25 01:24:44 +1300

    ci(*): add release step

---
+++ .releaserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'revert',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'minor',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    // '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/gitlab',
+  ],
+}
