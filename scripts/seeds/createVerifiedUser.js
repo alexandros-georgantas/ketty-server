d367154dcb9a8e9aba0bd6d8eb98a17b0ea78070
bela <bela.toci@protonmail.com>
2023-05-15 16:57:10 +0200

    fix:fixed lint error spaces for verified user script

--- scripts/seeds/createVerifiedUser.js
+++ scripts/seeds/createVerifiedUser.js
@@ -5,32 +5,31 @@ const User = require('../../models/user/user.model')
 
 ;
 
-(async ()=>{
-    try{
-      const email=process.argv[2]
-      const username=process.argv[3]
+(async () => {
+  try {
+    const email = process.argv[2]
+    const username = process.argv[3]
 
-      const newUser = await User.insert({
-        username,
-        password: 'Password@123',
-        agreedTc: true,
-        isActive: true,
-      })
+    const newUser = await User.insert({
+      username,
+      password: 'Password@123',
+      agreedTc: true,
+      isActive: true,
+    })
 
-      // eslint-disable-next-line no-console
-      console.log(newUser)
+    // eslint-disable-next-line no-console
+    console.log(newUser)
 
-      const verifiedUser = await Identity.insert({
-        userId: newUser.id,
-        email,
-        isVerified: true,
-        isDefault: true,
-      })
-    
-      // eslint-disable-next-line no-console
-      console.log(verifiedUser)
-      
-    }catch(err){
-      logger.error(err)
-    }
+    const verifiedUser = await Identity.insert({
+      userId: newUser.id,
+      email,
+      isVerified: true,
+      isDefault: true,
+    })
+
+    // eslint-disable-next-line no-console
+    console.log(verifiedUser)
+  } catch (err) {
+    logger.error(err)
+  }
 })()
