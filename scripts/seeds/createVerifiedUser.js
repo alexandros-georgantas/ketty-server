668da5d3189fc7125514a896ef118ba634189173
bela <bela.toci@protonmail.com>
2023-05-15 15:34:40 +0200

    fix:fixed lint error for verified user script

--- scripts/seeds/createVerifiedUser.js
+++ scripts/seeds/createVerifiedUser.js
@@ -1,28 +1,34 @@
 const { logger } = require('@coko/server')
 const Identity = require('@coko/server/src/models/identity/identity.model')
+
 const User = require('../../models/user/user.model')
 
-;(async ()=>{
+;
+
+(async ()=>{
     try{
       const email=process.argv[2]
       const username=process.argv[3]
 
-      const result = await User.insert({
-        username: username,
+      const newUser = await User.insert({
+        username,
         password: 'Password@123',
         agreedTc: true,
         isActive: true,
       })
-      console.log(result)
 
-      const random = await Identity.insert({
-        userId: result.id,
-        email: email,
+      // eslint-disable-next-line no-console
+      console.log(newUser)
+
+      const verifiedUser = await Identity.insert({
+        userId: newUser.id,
+        email,
         isVerified: true,
         isDefault: true,
       })
     
-      console.log(random)
+      // eslint-disable-next-line no-console
+      console.log(verifiedUser)
       
     }catch(err){
       logger.error(err)
