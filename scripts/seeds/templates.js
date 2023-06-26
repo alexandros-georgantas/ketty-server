c3010670585bcc3b170c5d6e7a35205b3fb666a3
Alexandros Georgantas <alexgeorg86@gmail.com>
2023-06-26 14:28:52 +0000

    feat(api/graphql): exporter functionality added, templates creation corrected

--- scripts/seeds/templates.js
+++ scripts/seeds/templates.js
@@ -1,8 +1,5 @@
 const { logger } = require('@coko/server')
 const find = require('lodash/find')
-const map = require('lodash/map')
-const isNil = require('lodash/isNil')
-const isEmpty = require('lodash/isEmpty')
 const get = require('lodash/get')
 const path = require('path')
 const fs = require('fs-extra')
@@ -11,90 +8,119 @@ const config = require('config')
 const { createTemplate, getTemplates } = require('../helpers/templates')
 
 const seedTemplates = async () => {
+  let templatesFolder
+
   try {
-    const normalizedTemplates = config.get('templates').map(t => ({
-      label: t.label.toLowerCase(),
-      url: t.url,
-      assetsRoot: t.assetsRoot.replace(/^\/+/, '').replace(/\/+$/, ''),
-    }))
+    const normalizedTemplates = config.has('templates')
+      ? config.get('templates').map(t => ({
+          label: t.label.toLowerCase(),
+          url: t.url,
+          assetsRoot: t.assetsRoot.replace(/^\/+/, '').replace(/\/+$/, ''),
+          supportedNoteTypes: t.supportedNoteTypes,
+        }))
+      : undefined
 
-    if (!isNil(normalizedTemplates) && !isEmpty(normalizedTemplates)) {
-      await getTemplates()
-
-      const templatesFolder = path.join(__dirname, '..', '..', 'templates')
-
-      const noteTypes = ['footnotes', 'endnotes', 'chapterEnd']
-
-      if (fs.existsSync(templatesFolder)) {
-        const fetchedTemplates = await fs.readdir(templatesFolder)
-        await Promise.all(
-          map(fetchedTemplates, async templateFolder => {
-            const sourceRoot = path.join(
-              __dirname,
-              '..',
-              '..',
-              'templates',
-              templateFolder,
-            )
-
-            const raw = fs.readFileSync(path.join(sourceRoot, 'template.json'))
-            const manifest = JSON.parse(raw)
-            const { name, author, target } = manifest
-
-            if (find(normalizedTemplates, { label: name.toLowerCase() })) {
-              logger.info(
-                '******* Create Templates script is starting ********',
-              )
-
-              const pagedData = {
-                name,
-                author,
-                target: 'pagedjs',
-              }
-
-              logger.info('PagedJS Templates')
-              await Promise.all(
-                map(noteTypes, async noteType =>
-                  createTemplate(
-                    sourceRoot,
-                    pagedData,
-                    get(target, 'pagedjs.file'),
-                    noteType,
-                  ),
-                ),
-              )
-
-              if (get(target, 'epub.file')) {
-                const epubData = {
-                  name,
-                  author,
-                  target: 'epub',
-                }
-
-                logger.info('EPUB Templates')
-                await Promise.all(
-                  map(noteTypes, async noteType =>
-                    createTemplate(
-                      sourceRoot,
-                      epubData,
-                      get(target, 'epub.file'),
-                      noteType,
-                    ),
-                  ),
-                )
-              }
-
-              logger.info(
-                '******* Create Templates script finished successfully ********',
-              )
-              await fs.remove(templatesFolder)
-              logger.info('******* Templates folder removed ********')
-            }
-          }),
-        )
-      }
+    if (!normalizedTemplates) {
+      logger.info('not templates defined in the config for automatic fetching')
+      return
     }
+
+    await getTemplates()
+
+    templatesFolder = path.join(__dirname, '..', '..', 'templates')
+
+    if (!fs.existsSync(templatesFolder)) {
+      throw new Error(
+        'something went wrong and your defined templates were not fetched correctly',
+      )
+    }
+
+    const fetchedTemplates = await fs.readdir(templatesFolder)
+
+    await Promise.all(
+      fetchedTemplates.map(async templateFolder => {
+        const sourceRoot = path.join(
+          __dirname,
+          '..',
+          '..',
+          'templates',
+          templateFolder,
+        )
+
+        const raw = fs.readFileSync(path.join(sourceRoot, 'template.json'))
+        const manifest = JSON.parse(raw)
+
+        const { name, author, target } = manifest
+
+        const templateConfig = find(normalizedTemplates, {
+          label: name.toLowerCase(),
+        })
+
+        if (!templateConfig) {
+          return
+        }
+
+        logger.info('******* Create Templates script is starting ********')
+
+        const pagedData = {
+          name,
+          author,
+          target: 'pagedjs',
+        }
+
+        if (
+          !templateConfig.supportedNoteTypes ||
+          templateConfig.supportedNoteTypes.length === 0
+        ) {
+          throw new Error(
+            'supportedNoteTypes is required for the creation of templates, please check your templates config',
+          )
+        }
+
+        const { supportedNoteTypes } = templateConfig
+
+        logger.info('PagedJS Templates')
+
+        await Promise.all(
+          supportedNoteTypes.map(async noteType =>
+            createTemplate(
+              sourceRoot,
+              pagedData,
+              get(target, 'pagedjs.file'),
+              noteType,
+            ),
+          ),
+        )
+
+        if (get(target, 'epub.file')) {
+          const epubData = {
+            name,
+            author,
+            target: 'epub',
+          }
+
+          logger.info('EPUB Templates')
+          await Promise.all(
+            supportedNoteTypes.map(async noteType =>
+              createTemplate(
+                sourceRoot,
+                epubData,
+                get(target, 'epub.file'),
+                noteType,
+              ),
+            ),
+          )
+        }
+
+        logger.info(
+          '******* Create Templates script finished successfully ********',
+        )
+      }),
+    )
+    await fs.remove(templatesFolder)
+    logger.info('******* Templates folder removed ********')
   } catch (e) {
+    fs.remove(templatesFolder)
     throw new Error(e.message)
   }
 }
