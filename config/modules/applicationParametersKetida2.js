953441e03e61094381f3c8b04635b3e51f36cb09
Grigor Malo <grigormalo@gmail.com>
2024-04-29 13:17:09 +0000

    refactor(*): remove configuration for uploading terms and conditions via a config file

--- config/modules/applicationParametersKetida2.js
+++ config/modules/applicationParametersKetida2.js
@@ -1,8 +1,4 @@
 const config = require('config')
-const fs = require('fs').promises
-const path = require('path')
-const marked = require('marked')
-const { logger } = require('@coko/server')
 
 const featureAIEnabled =
   (process.env.AI_ENABLED && JSON.parse(process.env.AI_ENABLED)) || false
@@ -11,150 +7,137 @@ const chatGPT = config.has('chatGPT') && config.get('chatGPT')
 
 const integrations = config.has('integrations') && config.get('integrations')
 
-module.exports = (async () => {
-  let termsAndConditions
-
-  try {
-    termsAndConditions = await fs.readFile(
-      path.join(__dirname, '../../config/termsAndConditions.md'),
-      'utf-8',
-    )
-  } catch (e) {
-    logger.error(e)
-  }
-
-  return {
-    instance: 'KETIDA_V2',
-    stages: [
-      {
-        title: 'Upload',
-        type: 'upload',
-      },
-      {
-        title: 'File Prep',
-        type: 'file_prep',
-      },
-      {
-        title: 'Edit',
-        type: 'edit',
-      },
-      {
-        title: 'Review',
-        type: 'review',
-      },
-      {
-        title: 'Clean Up',
-        type: 'clean_up',
-      },
-      {
-        title: 'Page Check',
-        type: 'page_check',
-      },
-      {
-        title: 'Final',
-        type: 'final',
-      },
-    ],
-    divisions: [
-      {
-        name: 'Frontmatter',
-        showNumberBeforeComponents: [],
-        allowedComponentTypes: [
-          {
-            value: 'component',
-            title: 'Component',
-            predefined: true,
-            visibleInHeader: true,
-          },
-          {
-            value: 'introduction',
-            title: 'Introduction',
-            predefined: true,
-            visibleInHeader: false,
-          },
-          {
-            value: 'preface',
-            title: 'Preface',
-            predefined: true,
-            visibleInHeader: false,
-          },
-          {
-            value: 'halftitle',
-            title: 'Half Title',
-            predefined: true,
-            visibleInHeader: false,
-          },
-          {
-            value: 'titlepage',
-            title: 'Title Page',
-            predefined: true,
-            visibleInHeader: false,
-          },
-          {
-            value: 'cover',
-            title: 'Cover',
-            predefined: true,
-            visibleInHeader: false,
-          },
-        ],
-        defaultComponentType: 'component',
-      },
-      {
-        name: 'Body',
-        showNumberBeforeComponents: ['part', 'chapter'],
-        allowedComponentTypes: [
-          {
-            value: 'part',
-            title: 'Part',
-            predefined: true,
-            visibleInHeader: true,
-          },
-          {
-            value: 'chapter',
-            title: 'Chapter',
-            predefined: true,
-            visibleInHeader: true,
-          },
-          {
-            value: 'unnumbered',
-            title: 'Unnumbered',
-            predefined: true,
-            visibleInHeader: true,
-          },
-        ],
-        defaultComponentType: 'chapter',
-      },
-      {
-        name: 'Backmatter',
-        showNumberBeforeComponents: [],
-        allowedComponentTypes: [
-          {
-            value: 'component',
-            title: 'Component',
-            predefined: true,
-            visibleInHeader: true,
-          },
-          {
-            value: 'appendix',
-            title: 'Appendix',
-            predefined: true,
-            visibleInHeader: false,
-          },
-          {
-            value: 'endnotes',
-            title: 'notes placeholder',
-            visibleInHeader: true,
-            predefined: true,
-          },
-        ],
-        defaultComponentType: 'component',
-      },
-    ],
-    lockTrackChangesWhenReviewing: false,
-    aiEnabled: featureAIEnabled,
-    chatGptApiKey: chatGPT?.key,
-    heartbeatInterval: process.env.WS_HEARTBEAT_INTERVAL || 5000,
-    integrations,
-    termsAndConditions: termsAndConditions && marked.parse(termsAndConditions),
-  }
-})()
+module.exports = {
+  instance: 'KETIDA_V2',
+  stages: [
+    {
+      title: 'Upload',
+      type: 'upload',
+    },
+    {
+      title: 'File Prep',
+      type: 'file_prep',
+    },
+    {
+      title: 'Edit',
+      type: 'edit',
+    },
+    {
+      title: 'Review',
+      type: 'review',
+    },
+    {
+      title: 'Clean Up',
+      type: 'clean_up',
+    },
+    {
+      title: 'Page Check',
+      type: 'page_check',
+    },
+    {
+      title: 'Final',
+      type: 'final',
+    },
+  ],
+  divisions: [
+    {
+      name: 'Frontmatter',
+      showNumberBeforeComponents: [],
+      allowedComponentTypes: [
+        {
+          value: 'component',
+          title: 'Component',
+          predefined: true,
+          visibleInHeader: true,
+        },
+        {
+          value: 'introduction',
+          title: 'Introduction',
+          predefined: true,
+          visibleInHeader: false,
+        },
+        {
+          value: 'preface',
+          title: 'Preface',
+          predefined: true,
+          visibleInHeader: false,
+        },
+        {
+          value: 'halftitle',
+          title: 'Half Title',
+          predefined: true,
+          visibleInHeader: false,
+        },
+        {
+          value: 'titlepage',
+          title: 'Title Page',
+          predefined: true,
+          visibleInHeader: false,
+        },
+        {
+          value: 'cover',
+          title: 'Cover',
+          predefined: true,
+          visibleInHeader: false,
+        },
+      ],
+      defaultComponentType: 'component',
+    },
+    {
+      name: 'Body',
+      showNumberBeforeComponents: ['part', 'chapter'],
+      allowedComponentTypes: [
+        {
+          value: 'part',
+          title: 'Part',
+          predefined: true,
+          visibleInHeader: true,
+        },
+        {
+          value: 'chapter',
+          title: 'Chapter',
+          predefined: true,
+          visibleInHeader: true,
+        },
+        {
+          value: 'unnumbered',
+          title: 'Unnumbered',
+          predefined: true,
+          visibleInHeader: true,
+        },
+      ],
+      defaultComponentType: 'chapter',
+    },
+    {
+      name: 'Backmatter',
+      showNumberBeforeComponents: [],
+      allowedComponentTypes: [
+        {
+          value: 'component',
+          title: 'Component',
+          predefined: true,
+          visibleInHeader: true,
+        },
+        {
+          value: 'appendix',
+          title: 'Appendix',
+          predefined: true,
+          visibleInHeader: false,
+        },
+        {
+          value: 'endnotes',
+          title: 'notes placeholder',
+          visibleInHeader: true,
+          predefined: true,
+        },
+      ],
+      defaultComponentType: 'component',
+    },
+  ],
+  lockTrackChangesWhenReviewing: false,
+  aiEnabled: featureAIEnabled,
+  chatGptApiKey: chatGPT?.key,
+  heartbeatInterval: process.env.WS_HEARTBEAT_INTERVAL || 5000,
+  integrations,
+  termsAndConditions: '',
+}
